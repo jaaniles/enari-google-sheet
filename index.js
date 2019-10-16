705d9927be71d5eebb34af69d54a2aa104a4eb2f
Jaani Leskinen <jaani.leskinen@fraktio.fi>
2019-10-16 23:08:09 +0300

    Fully working jynkker

--- index.js
+++ index.js
@@ -12,30 +12,29 @@ async.series([
         doc.useServiceAccountAuth(creds, step);
       },
     async function jynkInfoToSheet(step) {
-      console.log("hello")
       doc.getInfo(async function(err, info) {
-        sheet = info.worksheets.find(ws => ws.title === "Data")
-        sheet.setHeaderRow(['pId', 'name'], function(err, _sheet) {
-          console.log(err, _sheet)
+        sheet = info.worksheets.find(ws => ws.title === "asd")
+
+        sheet.setHeaderRow(['name', 'pId'], function(err, _sheet) {
+          if (err) {
+            console.log("error", error)
+          }
         })
-  
+
         let res = await axios.get("https://statsapi.web.nhl.com/api/v1/teams")
         let { data: { teams } } = res
   
         for (team of teams) {
+          console.log("Processing ", team.id)
           const res = await axios.get(`https://records.nhl.com/site/api/player/byTeam/${team.id}`)
           const { data: { data } } = res 
 
-          if (team.id !== 1) {
-            return
-          }
 
           for (player of data) {
-            console.log(player.id)
-            await sleep(150)
+            await sleep(200)
             await sheet.addRow({
-              pId: player.id,
-              name: player.fullName
+              name: player.fullName,
+              pId: player.id
             }, function(err, rows) {console.log("Error? ", err)})
           }
         }
